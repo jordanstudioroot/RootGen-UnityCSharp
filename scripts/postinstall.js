7994ef4509df4cdee72747919d87b0c9d5662f32
Jordan Nelson <jordannelson@protonmail.com>
2020-02-10 17:54:37 -0600

    Init project as NPM package. Move project files into src directory for package.

---
+++ scripts/postinstall.js
@@ -0,0 +1,33 @@
+var mkdirp = require('mkdirp');
+var path = require('path');
+var ncp = require('ncp');
+
+let packageName = '';
+
+// Package name
+if (argv[2]) {
+  packageName = argv[2];
+}
+else {
+  throw new Error("Missing arg to postinstall.js in package.json.");
+}
+
+// Paths
+var src = path.join(__dirname, '..', 'Assets', packageName);
+var dir = path.join(__dirname, '..', '..', '..', 'Assets', 'pkg-all', packageName);
+
+// Create folder if missing
+mkdirp(dir, function (err) {
+  if (err) {
+    console.error(err)
+    process.exit(1);
+  }
+
+  // Copy files
+  ncp(src, dir, function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+});
